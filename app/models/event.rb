8c1969c7f7e7115879cc49cb49823210aa57905e
joethyr <joethayr@gmail.com>
2022-05-27 20:53:34 -0600

    migrate creator references to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  # looks for the foreign key name after the association (creator_id)
+  belongs_to :creator, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_194413) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_28_024107) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_194413) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_194413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
