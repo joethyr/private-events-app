6cb8c864c69e5931d081fd22d600b38e26d28670
joethyr <joethayr@gmail.com>
2022-05-29 11:51:20 -0600

    create method works for Event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,22 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to events_url, notice: "Your event was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :location, :description, :creator_id)
+  end
 end
