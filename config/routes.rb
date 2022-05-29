8d49c32f5985200703f2a832c070462b3fa39952
joethyr <joethayr@gmail.com>
2022-05-29 11:28:38 -0600

    add form in event new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
   registrations: 'registrations'
   }
-
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  get 'events/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "events#index"
-
+  resources :events
 end
