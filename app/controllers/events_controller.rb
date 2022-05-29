2256ec31ddb0439837e471b33eca1b71796701a8
joethyr <joethayr@gmail.com>
2022-05-29 13:48:10 -0600

    add a view show file for Event object

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,15 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_event, only: %i[ show edit update destroy ]
   def index
     @events = Event.all
   end
 
   def new
     @event = Event.new
+  end
+
+  def show
   end
 
   def create
@@ -21,6 +25,10 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:title, :date, :location, :description, :creator_id)
