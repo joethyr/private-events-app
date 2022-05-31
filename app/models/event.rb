ce66294502d5d5869be2d97c72226c6a28315ac9
joethyr <joethayr@gmail.com>
2022-05-30 22:55:39 -0600

    finish app

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
   # looks for the foreign key name after the association (creator_id)
+  validates :title, :description, :date, :location, presence: true
   belongs_to :creator, class_name: "User"
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations
+
+  scope :past, -> { where('date < ?', Date.today).order(date: :desc) }
+  scope :future, -> { where('date > ?', Date.today).order(:date) }
 end
